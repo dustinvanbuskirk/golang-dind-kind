caf8cef151b866ea75f5855f5d1cfb47c423a29e
Dustin Van Buskirk <dev@vanbuskirk.me>
2021-03-16 13:28:47 -0700

    Update update-kube-config.sh

--- update-kube-config.sh
+++ update-kube-config.sh
@@ -1,4 +1,1 @@
-CONTAINER_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' kind-control-plane)
-HOST_PORT=$(docker inspect --format='{{(index (index .NetworkSettings.Ports "6443/tcp") 0).HostPort}}' kind-control-plane)
-
-sed -i "s/127.0.0.1/$CONTAINER_IP/g" $KUBECONFIG && sed -i "s/$HOST_PORT/6443/g" $KUBECONFIG
+sed -i -E -e 's/127.0.0.1|0\.0\.0\.0/docker/g' $KUBECONFIG
